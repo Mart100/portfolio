df80fe81e586c620d113608f3bee842b1604058c
Mart <martvanenck1@gmail.com>
2026-01-30 21:42:27 +0100

    formatting

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -172,7 +172,11 @@
 				stroke="currentColor"
 				class="h-4 w-4"
 			>
-				<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
+				/>
 			</svg>
 		</div>
 	</div>
